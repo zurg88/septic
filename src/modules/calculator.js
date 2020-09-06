3001846340ea998dd2d48111b37f163c43355dac
Gnatenko <gnatenko@gmail.com>
2020-09-06 23:29:23 +0300

    fixed calculator

--- src/modules/calculator.js
+++ src/modules/calculator.js
@@ -6,7 +6,18 @@ const calculator = () => {
 		firstWellDiameter = document.getElementById('first-well-diameter'),
 		secondWellDiameter = document.getElementById('second-well-diameter'),
 		firstWellQuantity = document.getElementById('first-well-quantity'),
-		secondWellQuantity = document.getElementById('second-well-quantity');
+		secondWellQuantity = document.getElementById('second-well-quantity'),
+		captureFormBtn = document.querySelector('.capture-form-btn'),
+		distanseToHomeInput = document.getElementById('distanseToHome'),
+		calcData = { type: 'однокамерный',
+					firstWellDiameter: '1.4',
+					 firstWellQuantity: '1',
+					 secondWellDiameter: '-',
+					 secondWellQuantity: '-',
+					 wellFloor: '-',
+					 distanseToHome: '-',
+					 totalCost: '13000'
+					};
 
 	let count1 = 1,
 		count2 = 0,
@@ -15,8 +26,7 @@ const calculator = () => {
 	calcResult.value = 13000;
 
 	const result = calcResult => {
-		console.log(calcResult.value);
-		return;
+		calcData.totalCost = calcResult.value;
 	};
 
 	const calcWellCheckbox = (checkbox, count, elem) => {
@@ -26,9 +36,15 @@ const calculator = () => {
 			if (count % 2 === 0) {
 				elem.style.display = 'block';
 				calcResult.value = +calcResult.value + 11000;
+				calcData.type = 'двухкамерный';
+				calcData.secondWellDiameter = '1.4';
+				calcData.secondWellQuantity = '1';
 			} else {
 				elem.style.display = 'none';
 				calcResult.value = +calcResult.value - 11000;
+				calcData.type = 'однокамерный';
+				calcData.secondWellDiameter = '-';
+				calcData.secondWellQuantity = '-';
 			}
 			result(calcResult);
 			return count;
@@ -37,6 +53,7 @@ const calculator = () => {
 
 	const ringsDiameterCalc = (sumResult, select, param1, sum1, param2, sum2) => {
 		select.addEventListener('change', () => {
+			calcData[select.name] = select.value;
 			if (select.value === param1) {
 				sumResult.value = +sumResult.value + +sum1;
 			} else if (select.value === param2) {
@@ -49,6 +66,9 @@ const calculator = () => {
 	const ringsQuantityCalc = (sumResult, select, sum) => {
 		const options = select.querySelectorAll('option');
 		select.addEventListener('change', () => {
+
+			calcData[select.name] = select.value;
+
 			for (let index = 0; index < options.length; index++) {
 
 				if (select.value === options[index].value) {
@@ -71,13 +91,31 @@ const calculator = () => {
 		wellFloorCheckbox.addEventListener('click', () => {
 			count3++;
 			if (count3 % 2 === 0) {
+				calcData[wellFloorCheckbox.name] = 'есть дно';
 				calcResult.value = +calcResult.value + 1000;
 			} else {
 				calcResult.value = +calcResult.value - 1000;
+				calcData[wellFloorCheckbox.name] = '-';
 			}
+			result(calcResult);
 		});
 	};
 
+	let distanseToHomeSum = 0;
+
+	const distanseToHome = sum => {
+		distanseToHomeInput.addEventListener('change', () => {
+			calcResult.value = +calcResult.value - distanseToHomeSum;
+			distanseToHomeSum = Number(sum * (distanseToHomeInput.value / 10));
+			calcResult.value = +calcResult.value + distanseToHomeSum;
+
+			calcData.distanseToHome = distanseToHomeInput.value;
+
+			result(calcResult);
+		});
+	};
+
+	distanseToHome(2000);
 	wellFloor();
 	ringsQuantityCalc(calcResult, firstWellQuantity, 3000);
 	ringsQuantityCalc(calcResult, secondWellQuantity, 4500);
